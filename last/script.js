758c45eb1848ae35db3e36f6890fe159bcc2c357
benJamin <67270125+handmade12@users.noreply.github.com>
2020-08-05 17:33:17 +0300

    Delete script.js

--- last/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-	const colorSelectorElement = document.querySelector('#colorSelector');
-	console.log(colorSelectorElement.value);
-	colorSelectorElement.onchange = () => {
-		const divElement = document.querySelector('div');
-		divElement.style.backgroundColor = colorSelectorElement.value;
-	};
-})
