a431dbb5644c84e63e7926eede49df7feba29f81
benJamin <67270125+handmade12@users.noreply.github.com>
2020-08-07 00:19:50 +0300

    Add files via upload

--- my/script.js
+++ my/script.js
@@ -1,14 +1,35 @@
-const switchThemeButtonElement = document.querySelector('#switchThemeButton');
-const darkThemeClass = 'dark';
+// Switch theme
+const switchThemeButtonElement = document.querySelector("#switchThemeButton");
+const darkThemeClass = "dark";
 
 function switchTheme() {
-    const bodyElement = document.querySelector('body');
-    const isDark = bodyElement.classList.contains(darkThemeClass);
-    if (isDark) {
-        bodyElement.classList.remove(darkThemeClass);
-    } else {
-        bodyElement.classList.add(darkThemeClass);
-    }
+  const bodyElement = document.querySelector("body");
+  const isDark = bodyElement.classList.contains(darkThemeClass);
+  if (isDark) {
+    bodyElement.classList.remove(darkThemeClass);
+  } else {
+    bodyElement.classList.add(darkThemeClass);
+  }
 }
 
-switchThemeButtonElement.addEventListener('click', switchTheme);
+switchThemeButtonElement.addEventListener("click", switchTheme);
+
+// Mobile menu
+const switchMobileMenuButtons = Array.from(
+  document.querySelectorAll(".switchMobileMenuButton")
+);
+const navVisibleClass = "visible";
+
+function toggleMobileMenu() {
+  const navElement = document.querySelector(".nav");
+  const isVisible = navElement.classList.contains(navVisibleClass);
+  if (isVisible) {
+    navElement.classList.remove(navVisibleClass);
+  } else {
+    navElement.classList.add(navVisibleClass);
+  }
+}
+
+switchMobileMenuButtons.forEach((button) => {
+  button.addEventListener("click", toggleMobileMenu);
+});
