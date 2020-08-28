f0093306479fbb6dd537be150ec085c153e7bf1b
Kirill <Kirilldibil02@gmail.com>
2020-08-28 17:02:32 +0300

    Поменял иконку и добавил немного bootstrapа

---
+++ Newsite/script.js
@@ -0,0 +1,14 @@
+const switchThemeButtonElement = document.querySelector("#switchThemeButton");
+const darkThemeClass = "dark";
+
+function switchTheme() {
+  const bodyElement = document.querySelector("body");
+  const isDark = bodyElement.classList.contains(darkThemeClass);
+  if (isDark) {
+    bodyElement.classList.remove(darkThemeClass);
+  } else {
+    bodyElement.classList.add(darkThemeClass);
+  }
+}
+
+switchThemeButtonElement.addEventListener("click", switchTheme);
